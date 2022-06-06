20795adc956a9b660b935cf084152c76449ec397
James <jamesballsemail@gmail.com>
2022-06-06 13:34:00 +0100

    Added UKFast module"

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        UKFast\HealthCheck\HealthCheckServiceProvider::class,
 
         /*
          * Application Service Providers...
