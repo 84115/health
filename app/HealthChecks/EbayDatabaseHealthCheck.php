9eb99fb9180f53e4dac72cd47689c6cd943d98ee
James <jamesballsemail@gmail.com>
2022-06-13 20:05:22 +0100

    db health

--- app/HealthChecks/EbayDatabaseHealthCheck.php
+++ app/HealthChecks/EbayDatabaseHealthCheck.php
@@ -7,7 +7,7 @@ use UKFast\HealthCheck\HealthCheck;
 class EbayDatabaseHealthCheck extends HealthCheck
 {
     public $name = 'worth-what-database';
-    public $domain = "http://api.worthwhat.com/ping";
+    public $domain = "http://api.worthwhat.com/ping/database";
 
     public function status()
     {
