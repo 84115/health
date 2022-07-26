1d4178fe718cde371cbdb079c97394d8c82377f3
James <jamesballsemail@gmail.com>
2022-07-26 19:43:49 +0100

    EverythingDailyHealthCheck

---
+++ app/HealthChecks/EverythingDailyHealthCheck.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\HealthChecks;
+
+use UKFast\HealthCheck\HealthCheck;
+
+class EverythingDailyHealthCheck extends HealthCheck
+{
+    public $name = 'jb-everything-daily';
+    public $domain = "https://everything-daily.co.uk/ping";
+
+    public function status()
+    {
+        $curlInit = curl_init($this->domain);
+
+        curl_setopt($curlInit, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($curlInit, CURLOPT_HEADER, true);
+        curl_setopt($curlInit, CURLOPT_NOBODY, true);
+        curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curlInit);
+
+        curl_close($curlInit);
+
+        if ($response) {
+            return $this->okay();
+        } else {
+            return $this->problem("Failed to connect to $this->domain");
+        }
+    }
+}
