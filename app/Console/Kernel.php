eb533f063ccf6aafbd129df10cf5a0f0c4aead66
James <jamesballsemail@gmail.com>
2022-09-12 14:04:53 +0100

    Increased Schedule To 15 Mins

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('health:try-all-enabled')->everyThirtyMinutes();
+        $schedule->call('health:try-all-enabled')->everyFifteenMinutes();
     }
 
     /**
