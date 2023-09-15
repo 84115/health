83bcfb6720f89d763e03d353ca28ef3886d0c8e8
James <jamesballsemail@gmail.com>
2023-09-15 14:18:01 +0100

    Refactor Existing Checks #2

--- app/HealthChecks/WorthWhat/StagingSiteHealthCheck.php
+++ app/HealthChecks/WorthWhat/StagingSiteHealthCheck.php
@@ -2,30 +2,10 @@
 
 namespace App\HealthChecks\WorthWhat;
 
-use UKFast\HealthCheck\HealthCheck;
+use App\HealthChecks\ArchivedHealthCheck;
 
-class StagingSiteHealthCheck extends HealthCheck
+class StagingSiteHealthCheck extends ArchivedHealthCheck
 {
     public $name = 'worth-what-staging-site';
     public $domain = "http://staging.worthwhat.com";
-
-    public function status()
-    {
-        $curlInit = curl_init($this->domain);
-
-        curl_setopt($curlInit, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($curlInit, CURLOPT_HEADER, true);
-        curl_setopt($curlInit, CURLOPT_NOBODY, true);
-        curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($curlInit);
-
-        curl_close($curlInit);
-
-        if ($response) {
-            return $this->okay();
-        } else {
-            return $this->problem("Failed to connect to $this->domain");
-        }
-    }
 }
